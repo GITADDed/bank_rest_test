9e1e2e9c03881261900a024fcb3be5efa8d6b37d
Sergey Makarov <kahu908@gmail.ocm>
2026-02-12 19:56:57 +0300

    Add request mapping on login controller

--- src/main/java/com/example/bankcards/controller/LoginController.java
+++ src/main/java/com/example/bankcards/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.example.bankcards.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class LoginController {
 
     @PostMapping("/auth/login")
