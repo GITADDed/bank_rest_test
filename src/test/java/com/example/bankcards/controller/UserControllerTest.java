95c9053e24d966b61701e0158e118d27879a0466
Sergey Makarov <kahu908@gmail.ocm>
2026-02-12 19:56:19 +0300

    Add User integration test

---
+++ src/test/java/com/example/bankcards/controller/UserControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.bankcards.controller;
+
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void createUser() throws Exception {
+        String requestJson = """
+                {
+                    "username": "user1",
+                    "password": "12345"
+                }
+                """;
+        String actualJson = mockMvc.perform(post("/api/v1/users")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        String expectedJson = """
+                {
+                "username": "user1",
+                "role": ["USER"]
+                }
+                """;
+        JSONAssert.assertEquals(expectedJson, actualJson, JSONCompareMode.LENIENT);
+    }
+}
