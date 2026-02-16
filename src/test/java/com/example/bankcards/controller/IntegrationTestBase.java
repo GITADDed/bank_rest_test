51b2cb6decaf3834a8f20e36c3eab2a6a9c1d09c
Sergei Makarov <kahu908@gmail.com>
2026-02-16 06:25:13 +0300

    Refactor database configuration to use Testcontainers for MySQL and update testcontainers-bom version to 1.19.8

--- src/test/java/com/example/bankcards/controller/IntegrationTestBase.java
+++ src/test/java/com/example/bankcards/controller/IntegrationTestBase.java
@@ -4,16 +4,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class IntegrationTestBase {
 
+    static MySQLContainer<?> mysql = SharedMySQLContainer.getInstance();
+
     @DynamicPropertySource
     static void props(DynamicPropertyRegistry r) {
-        r.add("spring.datasource.url", () -> "jdbc:mysql://localhost:3306/bankcards");
-        r.add("spring.datasource.username", () -> "root");
-        r.add("spring.datasource.password", () -> "root");
+        r.add("spring.datasource.url", mysql::getJdbcUrl);
+        r.add("spring.datasource.username", mysql::getUsername);
+        r.add("spring.datasource.password", mysql::getPassword);
     }
 }
 
