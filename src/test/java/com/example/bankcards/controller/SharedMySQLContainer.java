02df8c627f1a7eb16b6d12a1d72af610c45f183a
Sergei Makarov <kahu908@gmail.com>
2026-02-16 05:51:49 +0300

    Update testcontainers-bom version to 1.19.0 in pom.xml

--- src/test/java/com/example/bankcards/controller/SharedMySQLContainer.java
+++ src/test/java/com/example/bankcards/controller/SharedMySQLContainer.java
@@ -3,28 +3,22 @@ package com.example.bankcards.controller;
 import org.testcontainers.containers.MySQLContainer;
 
 public class SharedMySQLContainer extends MySQLContainer<SharedMySQLContainer> {
-    private static final String IMAGE_VERSION = "mysql:8";
-    private static SharedMySQLContainer container;
+
+    private static final SharedMySQLContainer INSTANCE =
+            new SharedMySQLContainer()
+                    .withDatabaseName("bankcards")
+                    .withUsername("bankcards_user")
+                    .withPassword("strong_password");
 
     private SharedMySQLContainer() {
-        super(IMAGE_VERSION);
+        super("mysql:8.4");
     }
 
     public static SharedMySQLContainer getInstance() {
-        if (container == null) {
-            container = new SharedMySQLContainer();
-            container.start();
-        }
-        return container;
+        return INSTANCE;
     }
 
-    @Override
-    public void start() {
-        super.start();
-    }
-
-    @Override
-    public void stop() {
-        // Не останавливаем контейнер, чтобы переиспользовать между тестами
+    static {
+        INSTANCE.start();
     }
 }
