2598d2437138098f9c0f6011d51b0a2182fb7ac0
Sergei Makarov <kahu908@gmail.com>
2026-02-16 05:18:45 +0300

    Refactor build configuration and remove Testcontainers annotation from IntegrationTestBase

--- src/test/java/com/example/bankcards/controller/IntegrationTestBase.java
+++ src/test/java/com/example/bankcards/controller/IntegrationTestBase.java
@@ -4,9 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class IntegrationTestBase {
